771152fac5661b06375d4d5488311cd3e96e863c
System <none>
2017-08-03 12:00:56 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
+
+// Import GraphQL schema
 const schema = require('./schema');
 
+
+// Express App Setup
 const app = express();
-
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
 app.get('/', bodyParser.json(), graphiqlExpress({ endpointURL: '/graphql' }));
 
+
+// Listen for connections
 const PORT = process.env.PORT || 3000;
 const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
